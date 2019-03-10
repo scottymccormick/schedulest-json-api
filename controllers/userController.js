6317a20815b4cac6ce9af299caf7fe108a1c4d72
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-10 12:54:23 -0700

    create user on post route.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router  = express.Router()
 
+const db = require('../models')
+
+// USER INDEX
 router.get('/', (req, res) => {
   res.send('Reached get route of user')
+})
+
+// USER CREATE
+router.post('/', async (req, res) => {
+  try {
+    const newUser = await db.User.create(req.body)
+    res.status(201).json(newUser)
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  }
 })
 
 module.exports = router
