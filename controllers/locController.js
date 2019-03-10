1e2a051859cd4f065e6b2cfc6bbb7a43c635c585
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-10 14:07:32 -0700

    init locations index route, controller file.

---
+++ controllers/locController.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router  = express.Router()
+
+const db = require('../models')
+
+router.get('/', (req, res) => {
+  res.send('found loc index route')
+})
+
+module.exports = router
