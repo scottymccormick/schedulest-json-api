dcb371df2feaae2c0b1e355f4e1c545525ee70a6
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-10 16:52:27 -0700

    location create route.

--- controllers/locController.js
+++ controllers/locController.js
@@ -3,8 +3,19 @@ const router  = express.Router()
 
 const db = require('../models')
 
+// LOC INDEX
 router.get('/', (req, res) => {
   res.send('found loc index route')
+})
+
+// LOC CREATE
+router.post('/', async (req, res) => {
+  try {
+    const newLocation = await db.Location.create(req.body)
+    res.json(newLocation)
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  }
 })
 
 module.exports = router
