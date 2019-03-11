5cf891f619de572ff209da7e6623cb87deb447c4
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-10 18:30:03 -0700

    install bcryptjs, set up register with hashed password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const router  = express.Router()
+const bcrypt  = require('bcryptjs')
 
 const db = require('../models')
+
+// USER REGISTER
+router.post('/register', async (req, res) => {
+  try {
+    const password = req.body.password
+    const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+    const userEntry = {}
+    userEntry.email = req.body.email
+    userEntry.password = hashedPassword
+    userEntry.name = req.body.name
+
+    const newUser = await db.User.create(userEntry)
+    res.json(newUser)
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  }
+})
 
 router.post('/login', (req, res) => {
   try {
