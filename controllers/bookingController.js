744c5bdb04821e76a9f70c7252483b384b80ebaf
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-10 17:39:27 -0700

    add delete booking route.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -45,4 +45,14 @@ router.put('/:id', async (req, res) => {
   }
 })
 
+// BOOKING DELETE
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedBooking = await db.Booking.findByIdAndDelete(req.params.id)
+    res.sendStatus(204)
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  }
+})
+
 module.exports = router
